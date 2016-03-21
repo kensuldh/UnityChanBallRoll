5c9de6d9cae089438052dec875f3d30b16666fd9
kensuke sakai <spiknsk@gmail.com>
2016-03-21 16:03:37 +0900

    FirstCommit

---
+++ Assets/_MyGame_kensuke/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private float moveHorizontal;
+    private float moveVertical;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input系はUpdateで
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // 実際に動かすのはFixedUpdateで
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // アイテムゲット
+        if (other.gameObject.CompareTag("Item"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
