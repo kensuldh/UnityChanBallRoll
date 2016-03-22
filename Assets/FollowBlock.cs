9074067cb55cb6309da4a81b136b71115ef87c15
kensuke sakai <spiknsk@gmail.com>
2016-03-22 17:40:53 +0900

    ステージを変更 ・Unityちゃんに合わせて動く床 ・スロープの後の四角筒

---
+++ Assets/FollowBlock.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowBlock : MonoBehaviour {
+
+    public GameObject player;
+    public float AxisLine;
+    private Vector3 offset;
+    private Vector3 posi;
+    public bool isLookAt;
+    private float translation;
+
+    // Use this for initialization
+    void Start () {
+        // 差を取っておく
+        offset = this.transform.position - player.transform.position;
+        posi = this.transform.position - player.transform.position;
+        this.posi.y += 0.5f;
+    }
+
+
+    void LateUpdate()
+    {
+        //this.translation = player.transform.position.z * 0.1f + offset.z;
+        //if (isLookAt)
+        //{
+        //    this.transform.Translate(0, translation, 0);
+        //}
+        //else {
+            // 毎フレーム差を足しておく
+        this.posi.z = player.transform.position.z - AxisLine;
+        this.transform.position = posi;
+        //}
+    }
+}
